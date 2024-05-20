4ecd92c10510fdd94f23f19f838d6732a662674f
Agnese Grike <minminn192@gmail.com>
2024-05-20 22:59:49 +0300

    done with header

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("header");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+window.onload = function() {document.getElementsByClassName("listItem").width = document.getElementsByClassName("sub-menu").width;}
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.scrollY > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
 const menu = document.getElementById("menu")
 let open;
 
