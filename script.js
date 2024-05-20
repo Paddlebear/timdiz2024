05a08879269dcfe7ffb97422c01d4a4ed55777af
Agnese Grike <minminn192@gmail.com>
2024-05-20 23:38:07 +0300

    more navbar shenanigans

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+
 
 // Get the header
 var header = document.getElementById("header");
@@ -7,26 +6,26 @@ var header = document.getElementById("header");
 // Get the offset position of the navbar
 var sticky = header.offsetTop;
 
-window.onload = function() {document.getElementsByClassName("listItem").width = document.getElementsByClassName("sub-menu").width;}
+window.onload = function () { document.getElementsByClassName("listItem").width = document.getElementsByClassName("sub-menu").width; }
 
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.scrollY > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
+// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if (window.scrollY > sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
 
 const menu = document.getElementById("menu")
 let open;
 
 function openMenu() {
-  if (open) {
-    menu.style.display = "none";
-    open = false;
-  } else if (!open) {
-    menu.style.display = "block";
-    open = true;
-  }
+    if (open) {
+        menu.style.display = "none";
+        open = false;
+    } else if (!open) {
+        menu.style.display = "block";
+        open = true;
+    }
 }
